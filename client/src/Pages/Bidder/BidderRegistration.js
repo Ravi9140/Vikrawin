bcb86177323300f5283e7f52ba0db5ae4a76c87e
jadhavganesh1409 <jadhavganesh14091999@gmail.com>
2022-05-10 15:46:44 +0530

    BidderRegistration and UpcomingEvents updated

--- client/src/Pages/Bidder/BidderRegistration.js
+++ client/src/Pages/Bidder/BidderRegistration.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 import { Box, Paper, Typography, Tab, Tabs } from "@mui/material";
 
@@ -55,62 +54,3 @@ const BidderRegistration = () => {
 };
 
 export default BidderRegistration;
-=======
-import React, { useState } from 'react'
-import {Box, Paper,Typography, Tab, Tabs} from '@mui/material';
-
-import Login from '../../Components/BidderLogin';
-import Signup from '../../Components/BidderSignup'; 
-const BidderRegistration=()=>{
-const [value,setValue]=useState(0)
-const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  const paperStyle={width:'70vw',height:'95vh',margin:"20px auto"}
-  function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-  
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box>
-            <Typography>{children}</Typography>
-          </Box>
-        )}
-      </div>
-    );
-  }
-  
-    return (
-        <Paper elevation={20} style={paperStyle}>
-        <Tabs
-          value={value}
-          indicatorColor="primary"
-          textColor="primary"
-          onChange={handleChange}
-          aria-label="disabled tabs example"
-        >
-          <Tab label="Sign In" />
-         
-          <Tab label="Sign Up" />
-        </Tabs>
-        <TabPanel value={value} index={0}>
-       <Login handleChange={handleChange}/>
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-      <Signup/>
-      </TabPanel>
-      </Paper>
-      
-    )
-}
-
-export default BidderRegistration;
->>>>>>> a8b400b363a8ec8982578b985d2e06fc7f07c920
