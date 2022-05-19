94bf10ec2c6b8cab504baf035b48e32666380c35
jadhavganesh1409 <jadhavganesh14091999@gmail.com>
2022-05-20 00:39:08 +0530

    static assests added

--- server.js
+++ server.js
@@ -35,6 +35,16 @@ app.use("/api/registeredbids", require("./routes/api/registeredBids"));
 app.use("/api/placebid", require("./routes/api/placeBid"));
 app.use("/api/endbid", require("./routes/api/endBidding"));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
