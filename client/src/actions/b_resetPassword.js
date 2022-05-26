6fea66852a83fdb4118febbe45b3ced3bc8cddd4
jadhavganesh1409 <jadhavganesh14091999@gmail.com>
2022-05-27 00:55:05 +0530

    action for reset password

---
+++ client/src/actions/b_resetPassword.js
@@ -0,0 +1,60 @@
+import { FORGOT_PASSWORD_REQUEST } from "./types";
+import axios from "axios";
+import { setAlert } from "./alert";
+const queryString = require("query-string");
+
+export const forgot_b_password =
+  ({ email }) =>
+  async (dispatch) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const body = JSON.stringify({
+      email,
+    });
+    try {
+      const res = await axios.post(
+        "api/bidder-reset/resetpassword",
+        body,
+        config
+      );
+      dispatch({
+        type: FORGOT_PASSWORD_REQUEST,
+      });
+      dispatch(setAlert(res.data.msg, res.data.type));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+export const reset_b_password =
+  ({ password }) =>
+  async (dispatch) => {
+    const parsed = queryString.parse(window.location.search);
+    const token = parsed.token;
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const body = JSON.stringify({
+      password,
+      token,
+    });
+    try {
+      const res = await axios.post(
+        "api/bidder-reset/updatepassword",
+        body,
+        config
+      );
+
+      dispatch({
+        type: FORGOT_PASSWORD_REQUEST,
+      });
+      dispatch(setAlert(res.data.msg, res.data.type));
+    } catch (err) {
+      console.log(err);
+    }
+  };
