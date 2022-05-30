2d095910f2591b7b136cd5030d210f045a0d3669
jadhavganesh1409 <jadhavganesh14091999@gmail.com>
2022-05-30 13:04:04 +0530

    Responsive textfield

--- client/src/Components/BidderLogin.jsx
+++ client/src/Components/BidderLogin.jsx
@@ -34,9 +34,16 @@ const BidderLogin = ({ loginbidder, isAuthenticatedBidder }) => {
   };
   const paperStyle = {
     padding: 10,
-    height: "65vh",
-    width: "40vw",
-    margin: "20px auto",
+    // alignSelf: "center",
+    // height: "65vh",
+    // minWidth: "45vw",
+    // maxWidth: "35vw",
+    // marginLeft: "50%",
+    // marginRight: "50%",
+    // "@media (max-width: 100px)": {
+    //   display: "none",
+    // },
+    margin: "20px  auto",
   };
   const avatarStyle = { backgroundColor: "#1bbd7e" };
   const btnstyle = { margin: "8px 0" };
@@ -52,60 +59,85 @@ const BidderLogin = ({ loginbidder, isAuthenticatedBidder }) => {
         {" "}
         <h2> Bidder </h2>{" "}
       </center>
-      <Grid>
-        <Paper elevation={10} style={paperStyle}>
-          <Grid align="center">
-            <Avatar style={avatarStyle}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <h2>Sign In</h2>
-          </Grid>
-          <form style={{ flexDirection: "row" }} onSubmit={(e) => onSubmit(e)}>
-            <TextField
-              label="Username"
-              name="email"
-              value={email}
-              onChange={(e) => onChange(e)}
-              placeholder="Enter username"
-              fullWidth
-            />
-            <TextField
-              style={{ marginTop: "15px" }}
-              label="Password"
-              name="password"
-              value={password}
-              onChange={(e) => onChange(e)}
-              placeholder="Enter password"
-              type="password"
-              fullWidth
-            />
-            <Button
-              type="submit"
-              color="primary"
-              variant="contained"
-              sx={{
-                marginTop: "20px",
-                borderRadius: "20px",
-                backgroundColor: "#1bbd7e",
-                color: "white",
-                "&:hover": {
-                  backgroundColor: "white",
-                  color: "black",
-                },
-              }}
-              fullWidth
+      <Grid
+        container
+        align="center"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item xs={9} sm={7} md={3} sx={{}}>
+          <Paper elevation={10} style={paperStyle}>
+            <Grid
+              item
+              align="center"
+              // md={3} xs={6}
             >
-              Sign in
-            </Button>
-            <Typography align="center" marginTop="20px">
-              <LINK to="/BidderForgotPassword">Forgot password ?</LINK>
-            </Typography>
-            {/* <Typography>
+              <Avatar style={avatarStyle}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <h2>Sign In</h2>
+            </Grid>
+            <Grid
+              item
+              // md={12} xs={12}
+            >
+              <form
+                style={{ flexDirection: "row" }}
+                onSubmit={(e) => onSubmit(e)}
+              >
+                <Grid item md={11} xs={11} sm={11}>
+                  <TextField
+                    label="Username"
+                    name="email"
+                    value={email}
+                    onChange={(e) => onChange(e)}
+                    placeholder="Enter username"
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item md={11} xs={11} sm={11}>
+                  <TextField
+                    style={{ marginTop: "15px" }}
+                    label="Password"
+                    name="password"
+                    value={password}
+                    onChange={(e) => onChange(e)}
+                    placeholder="Enter password"
+                    type="password"
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item>
+                  <Button
+                    type="submit"
+                    color="primary"
+                    variant="contained"
+                    sx={{
+                      marginTop: "20px",
+                      borderRadius: "20px",
+                      backgroundColor: "#1bbd7e",
+                      color: "white",
+                      "&:hover": {
+                        backgroundColor: "white",
+                        color: "black",
+                      },
+                    }}
+                    // fullWidth
+                  >
+                    Sign in
+                  </Button>
+                </Grid>
+                <Typography align="center" marginTop="20px">
+                  <LINK to="/BidderForgotPassword">Forgot password ?</LINK>
+                </Typography>
+                {/* <Typography>
               {" "}
               Don't have an account ?<Link href="#">Sign Up</Link>
             </Typography> */}
-          </form>
-        </Paper>
+              </form>
+            </Grid>
+          </Paper>
+        </Grid>
       </Grid>
     </>
   );
